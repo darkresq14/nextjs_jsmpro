08a7a1bb8b2a1ba3355043e3ef93b8dbe3a42f3d
Razvan Bielz <razvan.bielz@gmail.com>
2023-10-01 16:03:28 +0300

    add skeleton

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -1,7 +1,23 @@
+import { Skeleton } from '@/components/ui/skeleton';
 import React from 'react';
 
 const Loading = () => {
-  return <div>loading</div>;
+  return (
+    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
+      <section className="nav-padding w-full">
+        <Skeleton className="h-[274px] w-full rounded-lg bg-slate-700" />
+      </section>
+
+      <section className="mt-6 flex w-full flex-col sm:mt-20">
+        <Skeleton className="h-10 w-56 bg-slate-700" />
+        <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-slate-700" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-slate-700" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-slate-700" />
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Loading;
