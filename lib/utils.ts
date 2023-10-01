751ad1c8ff1a642f55885b9001ca74f165a198e2
Razvan Bielz <razvan.bielz@gmail.com>
2023-10-01 15:17:59 +0300

    fix UrlQueryParams type

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,6 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import qs from 'query-string';
-import { type } from 'os';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -11,25 +10,27 @@ type QueryParams = {
   params: string;
 };
 
-interface toAdd extends QueryParams {
+interface ToAdd extends QueryParams {
   key: string;
   value: string | null;
 }
 
-interface toRemove extends QueryParams {
+interface ToRemove extends QueryParams {
   keysToRemove: string[];
 }
 
-type UrlQueryParams = toAdd | toRemove;
+type UrlQueryParams =
+  | (ToAdd & { keysToRemove?: never })
+  | (ToRemove & { key?: never; value?: never });
 
 export function formUrlQuery(props: UrlQueryParams) {
   const currentUrl = qs.parse(props.params);
 
-  if ('keysToRemove' in props) {
+  if ('keysToRemove' in props && props.keysToRemove) {
     props.keysToRemove.forEach((keyToRemove) => {
       delete currentUrl[keyToRemove];
     });
-  } else if ('key' in props) {
+  } else {
     currentUrl[props.key] = props.value;
   }
 
